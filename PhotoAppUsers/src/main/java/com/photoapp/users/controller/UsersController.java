d9695f0b8438eacd9141ff08bc8238091c261e27
grootenthu <amit.patil60@outlook.com>
2021-10-22 14:16:19 +0530

    Subject : [P0010-5] Create a new Spring Boot API for users registration

--- PhotoAppUsers/src/main/java/com/photoapp/users/controller/UsersController.java
+++ PhotoAppUsers/src/main/java/com/photoapp/users/controller/UsersController.java
@@ -1,14 +1,19 @@
 package com.photoapp.users.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping (path = "/users/status")
 	public String status() {
-		return "Up & Running";
+		return "Up & Running on port - " + env.getProperty("local.server.port");
 	}
 	
 }
