b10cd2d807b10befa5225035c56f7eded58b0499
grootenthu <amit.patil60@yahoo.in>
2021-10-26 13:45:44 +0530

    Added logic to display all HTTP headers

--- PhotoAppUsers/src/main/java/com/photoapp/users/controller/UsersController.java
+++ PhotoAppUsers/src/main/java/com/photoapp/users/controller/UsersController.java
@@ -2,7 +2,9 @@ package com.photoapp.users.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +14,9 @@ public class UsersController {
 	private Environment env;
 	
 	@GetMapping (path = "/users/status")
-	public String status() {
+	public String status(@RequestHeader HttpHeaders httpHeaders) {
+		System.out.println("Headers : " + httpHeaders.toString() );
+		
 		return "Up & Running on port - " + env.getProperty("local.server.port");
 	}
 	
